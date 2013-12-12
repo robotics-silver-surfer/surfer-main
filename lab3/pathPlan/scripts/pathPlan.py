53e1e6a08b73d54282232749d40054c9b2843752
Ivan Buenfil <aibc213@gmail.com>
2013-12-12 01:02:30 -0600

    Reading Ref poop

--- lab3/pathPlan/scripts/pathPlan.py
+++ lab3/pathPlan/scripts/pathPlan.py
@@ -3,28 +3,41 @@ import roslib; roslib.load_manifest('pathPlan')
 import rospy
 from geometry_msgs.msg import Transform
 from hovercraft.msg import Gyro
-
+from Ref.msg import BotStatus
 
 
 
 def gyroCallback(data):
 
-	global startFlag 
+	global gyroFlag 
+	global gyro
+        gyro = data.angle
+	if gyroFlag is False:
+		gyroFlag = True
+
+def refCallback(data):
+
+	global startFlag
 	global goal
-	if startFlag is False:
-		goal.rotation.w = data.angle+90
-		startFlag = True
+	global gyroFlag 
+	global gyro
+
+	if gyroFlag is True and startFlag is False:
+		goal.rotation.w = gyro - data.currentPosition.rot
 
 if __name__ == '__main__':
 
+	global gyroFlag
 	global startFlag	
 	global goal 
 
 	startFlag = False
+        gyroFlag = False
 	goal = Transform()
 	goal.translation.z = 0.5
 	rospy.init_node('pathPlan')
 	rospy.Subscriber("hovercraft/Gyro", Gyro, gyroCallback)
+        rospy.Subscriber("hovercraft/Gyro", BotStatus, refCallback)
 	goalPub = rospy.Publisher('pathPlan/Command', Transform)
 
 	rospy.sleep(5)
