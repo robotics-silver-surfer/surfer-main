3eeb95df4a0735dbe68fc9d23d241fd03d846a96
Courtney Ingersoll <ingersollc@hotmail.com>
2013-12-11 18:14:02 -0600

    Cleaned version of the arbitrator.  Old arbitrator (from labs) saved as a separate file.

--- lab3/arbitrator/src/lab_arbitrator.cpp
+++ lab3/arbitrator/src/lab_arbitrator.cpp
@@ -20,8 +20,9 @@
 #define LIFT_OFF	0.0
 #define MANUAL_STATE	0
 #define AUTO_STATE	1
-#define TURN_R_STATE	2
-#define TURN_L_STATE	3
+#define TRIANGLE_STATE	2
+#define TURN_R_STATE	3
+#define TURN_L_STATE	4
 #define TURN_OFFSET	5.0
 
 //Xbox Button Constants
@@ -34,10 +35,6 @@
 #define XBOX_BACK_BTN	6
 #define XBOX_START_BTN  7
 #define XBOX_XBOX_BTN	8
-#define XBOX_DPAD_LEFT	11
-#define XBOX_DPAD_RIGHT	12
-#define XBOX_DPAD_UP	13
-#define XBOX_DPAD_DOWN	14
 
 //Xbox Joystick Constants
 #define XBOX_LS_X_AXIS	0
@@ -59,6 +56,8 @@ private:
 
   ros::Subscriber joy_sub_;
   ros::Subscriber joyAngleItgr_sub_;
+  ros::Subscriber triangle_sub_;
+  ros::Subscriber reactivectrl_sub_;
 
   ros::Publisher arb_pub_;
   ros::Publisher led_pub_;
@@ -72,14 +71,16 @@ private:
   //Class functions
   void joyCallback( const sensor_msgs::Joy::ConstPtr& joy );
   void joyAngleItgrCallback( const geometry_msgs::Quaternion& itgr );
-  void publishControlState( int state_change );
-  void signalLED( hovercraft::LED& led, bool green, bool red, int blink );
-  void stateChange( int state_change );
+  void triangleCallback( const geometry_msgs::Transform& triangle );
+  void reactiveCtrlCallback( const geometry_msgs::Transform& reactiveCtrl ); 
+  void publishControlState( int state );
+  void signalLED( hovercraft::LED& led, bool green, int blink );
+  void stateChange( int state );
 
   //Class variables
   bool lift_on;
   int state;
-  int auto_state;
+  int previous_state;
 
 }; /* end class declaration */
 
@@ -91,6 +92,8 @@ Arbitrator::Arbitrator()
   //Set subscribers and publishers
   joy_sub_ = nh_.subscribe("joy", 10, &Arbitrator::joyCallback, this);
   joyAngleItgr_sub_ = nh_.subscribe("joyAngleIntegrater/Data", 10, &Arbitrator::joyAngleItgrCallback, this);
+  triangle_sub_ = nh_.subscribe("triangle/Data", 10, &Arbitrator::triangleCallback, this);
+  reactivectrl_sub_ = nh_.subscribe("reactivecontrol/Control", 10, &Arbitrator::reactiveCtrlCallback, this);
 
   arb_pub_ = nh_.advertise<geometry_msgs::Transform>("arbitrator/Data", 1);
   ctrl_pub_ = nh_.advertise<reactivecontrol::Control>("arbitrator/Control", 1);
@@ -99,6 +102,7 @@ Arbitrator::Arbitrator()
   //Set class variables
   lift_on = false;
   state = MANUAL_STATE;
+  previous_state = MANUAL_STATE;
 
 } /* end class constructor */
 
@@ -139,11 +143,13 @@ void Arbitrator::joyCallback( const sensor_msgs::Joy::ConstPtr& joy )
   {
     if( state == MANUAL_STATE )
     {
+      previous_state = state;
       state = AUTO_STATE;
       stateChange( state );
     }
     else
     {
+      previous_state = state;
       state = MANUAL_STATE;
       stateChange( state );
     }
@@ -151,28 +157,52 @@ void Arbitrator::joyCallback( const sensor_msgs::Joy::ConstPtr& joy )
   }
 
   //Automatic right rotation
-  if( float( joy->buttons[XBOX_R_BUMPER] ) == 1 )
+  if( float( joy->buttons[XBOX_R_BUMPER] ) == 1 &&
+      state != TRIANGLE_STATE )
   {
+    previous_state = state;
     state = TURN_R_STATE;
     stateChange( state );
   }
 
   //Automatic left rotation
-  if( float( joy->buttons[XBOX_L_BUMPER] ) == 1 )
+  if( float( joy->buttons[XBOX_L_BUMPER] ) == 1 &&
+      state != TRIANGLE_STATE )
   {
+    previous_state = state;
     state = TURN_L_STATE;
     stateChange( state );
   }
 
+  //Automatic triangle execution (only allowed in auto mode)
+  if( float( joy->buttons[XBOX_Y_BTN] ) == 1 )
+  {
+    //return to previous state
+    if( state == TRIANGLE_STATE )
+    {
+      state = previous_state;
+      stateChange( state );
+    }
+    //begin triangle execution
+    else
+    {
+      previous_state = state;
+      state = TRIANGLE_STATE;
+      stateChange( state );
+    }
+
+  }
+
   //Update current translational motion
   arb_data.translation.x = joy->axes[XBOX_RS_X_AXIS];
+  if( state != TRIANGLE_STATE )
+  {
+    arb_data.translation.y = joy->axes[XBOX_RS_Y_AXIS];
+  }
 
   //Turn on/off LEDs according to controller buttons
-  if( state != AUTO_STATE )
-  {
-    led_on.led33_green = float( joy->buttons[XBOX_A_BTN] );
-    led_on.led33_red = float( joy->buttons[XBOX_B_BTN] );
-  }
+  led_on.led33_green = float( joy->buttons[XBOX_A_BTN] );
+  led_on.led33_red = float( joy->buttons[XBOX_B_BTN] );
 
   arb_pub_.publish( arb_data );
   led_pub_.publish( led_on );
@@ -187,7 +217,7 @@ void Arbitrator::joyCallback( const sensor_msgs::Joy::ConstPtr& joy )
  */
 void Arbitrator::joyAngleItgrCallback( const geometry_msgs::Quaternion& itgr )
 {
-  if( state == MANUAL_STATE )
+  if( state != TRIANGLE_STATE)
   {
     arb_data.rotation.w = itgr.w;
     arb_pub_.publish( arb_data );
@@ -197,15 +227,47 @@ void Arbitrator::joyAngleItgrCallback( const geometry_msgs::Quaternion& itgr )
 
 //---------------------------------------------------------------------
 
-/* Arbitrator::publishControlState()
- *   Publishes the current states of the arbitrator.
+/* Arbitrator::triangleCallback()
+ *  Updates the target angle and forward momentum for the PID
+ *  as directed by the triangle node.
  */
-void Arbitrator::publishControlState( int state_change )
+void Arbitrator::triangleCallback( const geometry_msgs::Transform& triangle )
 {
-  if( state_change >= 0 )
+  if( state == TRIANGLE_STATE )
   {
-    ctrl_state.state = state;
+    arb_data.translation.y = triangle.translation.y;
+    arb_data.rotation.w = triangle.rotation.w;
+    arb_pub_.publish( arb_data );
   }
+
+} /* end method Arbitrator::triangleCallback() */
+
+//---------------------------------------------------------------------
+
+/* Arbitrator::reactiveCtrlCallback()
+ *   Updates the forward momentum for the PID as directed
+ *   by the reactivecontrol node.
+ */
+void Arbitrator::reactiveCtrlCallback( const geometry_msgs::Transform& reactiveCtrl )
+{
+
+  if( state == AUTO_STATE )
+  {
+    arb_data.translation.y = reactiveCtrl.translation.y;
+    arb_pub_.publish( arb_data );
+  }
+
+} /* end method Arbitrator::reactiveCtrlCallback() */
+
+//---------------------------------------------------------------------
+
+/* Arbitrator::publishControlState()
+ *   Publishes the current control state of the arbitrator.
+ */
+void Arbitrator::publishControlState( int state )
+{
+
+  ctrl_state.state = state;
   ctrl_pub_.publish( ctrl_state );
 
 } /* end method Arbitrator::publishControlState() */
@@ -215,56 +277,34 @@ void Arbitrator::publishControlState( int state_change )
 /* Arbitrator::signalLED()
  *   Flashes an LED on the hovercraft.
  */
-void Arbitrator::signalLED( hovercraft::LED& led, bool green, bool red, int blink )
+void Arbitrator::signalLED( hovercraft::LED& led, bool green, int blink )
 {
-  //start with LEDs off
-  led.led33_green = LED_OFF;
-  led.led33_red = LED_OFF;
-  led_pub_.publish( led_on );
-
   int i;
-  if( blink > 0 )
+  for( i = 0; i < blink; i++ )
   {
-    for( i = 0; i < blink; i++ )
+    ros::Duration(0.2).sleep(); //sleep for 20ms
+    if( green )
     {
-      ros::Duration(0.2).sleep(); //sleep for 20ms
-      if( green )
-      {
-        led.led33_green = LED_ON;
-      }
-      if( red )
-      {
-        led.led33_red = LED_ON;
-      }
-    
-      led_pub_.publish( led_on );
-
-      ros::Duration(0.2).sleep(); //sleep for 20ms
-      if( green )
-      {
-        led.led33_green = LED_OFF;
-      }
-      if( red )
-      {
-        led.led33_red = LED_OFF;
-      }
-    
-      led_pub_.publish( led_on );
+      led.led33_green = LED_ON;
     }
-  }
-  //Negative blinks = hold LED on
-  else if( blink < 0 )
-  {
-      if( green )
-      {
-        led.led33_green = LED_ON;
-      }
-      if( red )
-      {
-        led.led33_red = LED_ON;
-      }
+    else
+    {
+      led.led33_red = LED_ON;
+    }
+    
+    led_pub_.publish( led_on );
 
-      led_pub_.publish( led_on );
+    ros::Duration(0.2).sleep(); //sleep for 20ms
+    if( green )
+    {
+      led.led33_green = LED_OFF;
+    }
+    else
+    {
+      led.led33_red = LED_OFF;
+    }
+    
+    led_pub_.publish( led_on );
   }
 
 } /* end method Arbitrator::signalLED() */
@@ -275,21 +315,36 @@ void Arbitrator::signalLED( hovercraft::LED& led, bool green, bool red, int blin
  *   Displays a message on the console output and with the
  *   hovercraft LEDs to signal the current control state.
  */
-void Arbitrator::stateChange( int state_change )
+void Arbitrator::stateChange( int state )
 {
-  publishControlState( state_change );
-  switch( state_change )
+  publishControlState( state );
+  switch( state )
   {
     case MANUAL_STATE:
       ROS_INFO("State = Manual");
       ROS_INFO("  --> Press Back button to return to Auto");
-      signalLED( led_on, true, false, 3 ); //flash green led 3 times
+      ROS_INFO("  --> Press Y button to begin Triangle");
+      signalLED( led_on, true, 3 ); //flash green led 3 times
       break;
     case AUTO_STATE:
       ROS_INFO("State = Auto");
       ROS_INFO("  --> Press Back button to return to Manual");
-      signalLED( led_on, false, true, 3 ); //flash red led 3 times
-  } //end switch state_change
+      ROS_INFO("  --> Press Y button to begin Triangle");
+      signalLED( led_on, false, 3 ); //flash red led 3 times
+      break;
+    case TRIANGLE_STATE:
+      ROS_INFO("State = Triangle");
+      ROS_INFO("  --> Press Y button to end Triangle");
+      signalLED( led_on, true, 1 ); //flash led green, red, green pattern
+      signalLED( led_on, false, 1 );
+      signalLED( led_on, true, 1 );
+      break;
+    case TURN_R_STATE:
+    case TURN_L_STATE:
+      state = previous_state;
+    default:
+      break;
+  }
 
 } /* end method Arbitrator::stateChange() */
 
